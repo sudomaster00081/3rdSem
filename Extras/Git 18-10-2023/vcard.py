0acce82372ad0f4e5ff9a8d48ad1eab7f6ce9d40
sudomaster00081 <ajayprasadd0008@gmail.com>
2023-11-19 23:06:53 +0530

    +Added Git class process +Added Review PAper Network Intrusion Detection Systems +Added BAse LateX Presentation +Added Basic Structure to PPT

--- Extras/Git 18-10-2023/vcard.py
+++ Extras/Git 18-10-2023/vcard.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import vobject
 
-#pip install vobject
-
+#pip install vobject pandas numpy openpyxl
 
 # Read the Excel file into a DataFrame
 file_path = 'your_excel_file.xlsx'
@@ -11,9 +10,9 @@ df = pd.read_excel(file_path)
 # Filter the DataFrame based on the condition (Year != '2nd Year')
 filtered_df = df[df['Year'] != '2nd Year']
 
-# Create a vCard for each entry and save to a vCard file
-vcard_file_path = 'git_ppls.vcard'
-with open(vcard_file_path, 'w') as vcard_file:
+# Create a vCard for each entry and save to a VCF file
+vcf_file_path = 'git_ppls.vcf'
+with open(vcf_file_path, 'w', encoding='utf-8') as vcf_file:
     for index, row in filtered_df.iterrows():
         full_name = row['Full Name']
         batch = row['Batch']
@@ -24,6 +23,6 @@ with open(vcard_file_path, 'w') as vcard_file:
         vcard.add('FN').value = contact_card
 
         # Write the vCard to the file
-        vcard_file.write(vcard.serialize())
+        vcf_file.write(vcard.serialize())
 
-print(f"Contact cards saved to {vcard_file_path}")
+print(f"Contact cards saved to {vcf_file_path}")
